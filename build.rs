6aece5c2c79da9594770899a13d6b37df68acde1
Michael Lodder <redmike7@gmail.com>
2018-05-02 16:44:48 -0600

    Fixed missing paren

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     let zmq_has = match env::var("ZMQ_HAS") {
         Ok(value) => value,
-        Err(..) => panic!("Please set the ZMQ_HAS environment variable (e.g. \"ipc,pgm,tipc,norm,curve,gssapi\"")
+        Err(..) => panic!("Please set the ZMQ_HAS environment variable (e.g. \"ipc,pgm,tipc,norm,curve,gssapi\")")
     };
 
     println!("rerun-if-env-changed=ZMQ_HAS");
